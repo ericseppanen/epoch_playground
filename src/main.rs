a01da39f1eb0ed71d7fd3ecd4a39ad806c316eca
Eric Seppanen <eds@reric.net>
2019-11-17 14:30:35 -0800

    play around with guard.defer
    One of the critical parts about epoch-based memory management is the
    concept of "deferred" work.  The main thing we want is probably deleting
    released objects, but crossbeam will let you call arbitrary functions.
    
    crossbeam::epoch puts the interesting calls into the Guard object, so
    let's play around with Guard::defer().  It looks like the default
    crossbeam behavior is to accumulate a lot of work on the local thread
    before pushing that work to the global Collector, which will somehow
    trigger the actual running of the deferred work.  There's a
    Guard::flush() call to do that pushing immediately, if you're impatient.
    
    It's also worth noting that in our small example, there in deferred work
    that never runs-- our program simply ends.  I doubt that's a real
    problem (who cares if we failed to free a bunch of data right before the
    process exits), but maybe we'll figure that out later.

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,36 @@ impl Drop for Canary {
     }
 }
 
-fn main() {
-    let a = Atomic::new(Canary::new("first"));
+struct BirdCage {
+    c: Atomic<Canary>,
+}
 
+fn cleanup(n: usize) {
+    println!("[{}] doing deferred cleanup", n);
+}
+
+fn access(birdcage: &BirdCage, n: usize) {
     let guard = &pin();
-    let shared = a.load(Ordering::SeqCst, guard);
+    let shared = birdcage.c.load(Ordering::SeqCst, guard);
     let c: &Canary = unsafe{shared.as_ref()}.unwrap();
-    println!("accessing {}", c.name);
+    println!("[{}] accessing {}", n, c.name);
+    guard.defer(move || {
+        cleanup(n);
+    });
+
+    // Uncomment this to see the deferred function run sooner.
+    // Otherwise, the default Collector will wait until a bunch of
+    // deferred actions have accumulated (~256 in crossbeam 0.7.3).
+
+    //guard.flush();
+}
+
+fn main() {
+    let birdcage = BirdCage {
+        c: Atomic::new(Canary::new("first")),
+    };
+    // Increase this number to see the deferred functions run.
+    for n in 0..10 {
+        access(&birdcage, n);
+    }
 }
