40ac17eec89cdeedaecac291d3bdd37c8233ea4b
Eric Seppanen <eds@reric.net>
2019-11-17 10:40:05 -0800

    build a Canary struct
    It will be useful to have a type that announces its own destruction.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+
+#[derive(Debug)]
+struct Canary {
+    name: String,
+}
+
+impl Canary {
+    fn new(name: &str) -> Canary {
+        Canary {
+            name: name.to_owned(),
+        }
+    }
+}
+
+impl Drop for Canary {
+    fn drop(&mut self) {
+        println!("{}: dropped", self.name);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _a = Canary::new("first");
 }
