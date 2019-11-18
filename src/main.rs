3b33cdd9d357bf3e8b45d61d08dd6b943886fd3a
Eric Seppanen <eds@reric.net>
2019-11-17 18:00:22 -0800

    add a replace() method
    If we're going to do concurrent operations requiring memory management,
    we're going to need a way to actually remove things from our data
    structure (BirdCage).
    
    Add a replace() method, which goes into our data structure and removes
    one Canary and replaces it with another.  We use Atomic::swap() to do
    this.
    
    Note we have removed the Canary pointer from the main data structure,
    but we haven't actually done anything to free it.  Each time replace()
    runs, it leaks one Canary.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crossbeam::epoch::{pin, Atomic};
+use crossbeam::epoch::{pin, Atomic, Owned};
 use std::sync::atomic::Ordering;
 
 #[derive(Debug)]
@@ -41,6 +41,24 @@ impl BirdCage {
         let shared = self.c[n].load(Ordering::SeqCst, guard);
         let c: &Canary = unsafe{shared.as_ref()}.unwrap();
         println!("[{}] accessing {}", ctx, c.name);
+    }
+
+    fn replace(&self, n: usize, ctx: &str, new_c: Canary) {
+        println!("[{}] put {} into slot {}", ctx, new_c.name, n);
+
+        let guard = &pin();
+
+        // swap() will only accept a Shared or Owned, so let's make one of those.
+        // There are multiple ways to write this code but Owned seems to signal
+        // my intent (because at this point I'm the sole owner.)
+        let owned_new_c = Owned::new(new_c);
+
+        // We are stealing whatever Canary happens to be present in this
+        // location, and substituting a new one.
+        let stolen_c = self.c[n].swap(owned_new_c, Ordering::SeqCst, guard);
+        let c: &Canary = unsafe{stolen_c.as_ref()}.unwrap();
+        println!("[{}] removed {}", ctx, c.name);
+
         let defer_ctx = ctx.to_owned();
         guard.defer(move || {
             cleanup(defer_ctx);
@@ -52,6 +70,8 @@ impl BirdCage {
 
         //guard.flush();
     }
+
+
 }
 
 fn cleanup(ctx: String) {
@@ -65,5 +85,9 @@ fn main() {
     // Increase this number to see the deferred functions run.
     for n in 0..bc_size {
         birdcage.access(n, "main");
+    }
+    for n in 0..bc_size {
+        let c = Canary::new(&format!("Cuckoo {}", n));
+        birdcage.replace(n, "main", c);
     }
 }
